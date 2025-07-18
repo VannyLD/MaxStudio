20638e83df4e44670ad1090edec07f339bf223c3
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-07-18 18:06:58 +0700

    Update and rename All in One v1.0.9.bat to All in One v1.1.9.bat

---
+++ All in One v1.1.9.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Closing current application...
+taskkill /IM "Max_Studio v1.0.9.exe" /F
+timeout /t 2 /nobreak
+
+echo Downloading new version...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MaxStudio/releases/download/v1.1.9/Max.Studio.exe', 'Max_Studio.exe')"
+
+echo Starting new version...
+start Max_Studio.exe
+
+echo Cleaning up...
+del "%~f0"

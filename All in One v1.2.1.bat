f214bd22d9793fb1538cadd8c5f7331ea86cee16
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-08-02 03:58:49 +0700

    Update and rename All in One v1.2.0.bat to All in One v1.2.1.bat

---
+++ All in One v1.2.1.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Closing current application...
+taskkill /IM "Max_Studio.exe" /F
+timeout /t 2 /nobreak
+
+echo Downloading new version...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MaxStudio/releases/download/v1.2.1/Max.Studio_v1.2.1.exe', 'Max_Studio_v1.2.0.exe')"
+
+echo Starting new version...
+start Max_Studio_v1.2.0.exe
+
+echo Cleaning up...
+del "%~f0"
